655198463084f5304ed2b16ea5d46ef03dfea9ed
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2016-08-19 18:11:14 +0200

    installa redis, kibana, el

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,38 @@
 #!/bin/bash -eux
 
 # Install EPEL repository.
-yum -y install epel-release
+yum -y install epel-release wget
 
-# Install Ansible.
-yum -y install ansible
+wget -O /tmp/jdk-8u102-linux-x64.rpm --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm
+rpm -ivh /tmp/jdk-8u102-linux-x64.rpm
+
+yum install -y redis
+chkconfig redis on
+service redis start
+
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+cat<<EOF > /etc/yum.repos.d/elasticsearch.repo
+[elasticsearch-2.x]
+name=Elasticsearch repository for 2.x packages
+baseurl=https://packages.elastic.co/elasticsearch/2.x/centos
+gpgcheck=1
+gpgkey=https://packages.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+EOF
+yum install -y elasticsearch
+chkconfig --add elasticsearch
+service elasticsearch start
+
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+cat<<EOF > /etc/yum.repos.d/kibana.repo
+[kibana-4.5]
+name=Kibana repository for 4.5.x packages
+baseurl=http://packages.elastic.co/kibana/4.5/centos
+gpgcheck=1
+gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+EOF
+yum install -y kibana
+chkconfig --add kibana
+service kibana start
+
